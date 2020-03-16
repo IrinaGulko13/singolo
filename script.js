fb021e5db6e0aeddf33100416a45cdb129aa792d
Iryna Hulko <irinagulko13@gmail.com>
2020-03-16 10:39:45 +0300

    feat:add form task

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const MENU = document.getElementById('menu');
 const PORTFOLIO = document.getElementById('portfolio');
 const PICTURES = document.getElementById('pic');
+const CL_BTN = document.getElementById('close-btn');
+const FORM = document.getElementById('form-elem');
+const SUBJECT = document.querySelector('.input-subject');
+const DESC = document.querySelector('.input-desc');
+const MODAL = document.querySelector('.message-block');
+
+
 
 MENU.addEventListener('click', (event) => {
     MENU.querySelectorAll('a').forEach(el => el.classList.remove('active'))
@@ -16,3 +23,23 @@ PICTURES.addEventListener('click', (event) => {
     PORTFOLIO.querySelectorAll('img').forEach(el => el.classList.remove('active'));
     event.target.classList.add('active');
 });
+
+FORM.addEventListener('submit', (event) => {
+    event.preventDefault();
+    MODAL.hidden = false;
+
+    if (!SUBJECT.value) MODAL.querySelector('.message p:nth-child(2)').insertAdjacentHTML('beforeend', '<span id = "theme-in-modal">Без темы</span>');
+    else {
+        MODAL.querySelector('.message p:nth-child(2)').insertAdjacentHTML('beforeend', `<span id = "theme-in-modal"><strong>Тема: </strong>${SUBJECT.value}</span>`);
+    }
+
+    if (!DESC.value) MODAL.querySelector('.message p:nth-child(3)').insertAdjacentHTML('beforeend', '<span id = "desc-in-modal">Без описания</span>');
+    else {
+        MODAL.querySelector('.message p:nth-child(3)').insertAdjacentHTML('beforeend', `<span id = "desc-in-modal"><strong>Описание: </strong>${DESC.value}</span>`);
+    }
+});
+
+CL_BTN.addEventListener('click', function(event) {
+    MODAL.hidden = true;
+});
+
